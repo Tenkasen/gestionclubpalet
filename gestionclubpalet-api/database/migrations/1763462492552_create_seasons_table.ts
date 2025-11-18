793fa66b991929cd149ae6ebf84242fa351a9e04
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-11-18 11:53:19 +0100

    feat : seasons migration file

---
+++ gestionclubpalet-api/database/migrations/1763462492552_create_seasons_table.ts
@@ -0,0 +1,22 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'seasons'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('nom').notNullable()
+      table.enum('type', ['ENTRAINEMENT', 'CHAMPIONNAT', 'COUPE']).notNullable()
+      table.date('date_debut').notNullable()
+      table.date('date_fin')
+      table.integer('club_id')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
