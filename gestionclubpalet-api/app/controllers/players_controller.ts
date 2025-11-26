9e92588002891bf6f9b25e94de106522a4c76a84
Mickael-Bourdet <mickael.brdt@gmail.com>
2025-11-26 19:46:57 +0100

    feat : register function

--- gestionclubpalet-api/app/controllers/players_controller.ts
+++ gestionclubpalet-api/app/controllers/players_controller.ts
@@ -1,6 +1,7 @@
 import Player from '#models/player'
 import SeasonRegistration from '#models/season_registration'
 import type { HttpContext } from '@adonisjs/core/http'
+import { error } from 'console'
 
 export default class PlayersController {
   async index({ request }: HttpContext) {
@@ -26,5 +27,27 @@ export default class PlayersController {
     const data = request.only(['nom', 'prenom', 'isGuest', 'clubId'])
     const player = await Player.create(data)
     return player
+  }
+
+  async register({ params, request, response }: HttpContext) {
+    const { playerId } = request.only(['playerId'])
+
+    // check existing user
+    const existingPlayer = await SeasonRegistration.query()
+      .where('seasonId', params.seasonId)
+      .where('playerId', playerId)
+      .first()
+
+    if (existingPlayer) {
+      return response.conflict({ error: 'Ce joueur est déjà inscrit pour cette saison' })
+    }
+
+    const registration = await SeasonRegistration.create({
+      seasonId: params.seasonId,
+      playerId: playerId,
+    })
+
+    await registration.load('player')
+    return response.created(registration)
   }
 }
