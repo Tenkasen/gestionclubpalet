6b74213a96e49dc87400cf0cfde024042cb93bf3
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-02-26 16:47:19 +0100

    feat : create season API layer

---
+++ gestionclubpalet-front/src/api/season.api.ts
@@ -0,0 +1,23 @@
+import type { ISeason } from "../types";
+import { api } from "./client";
+
+export const seasonsAPI = {
+  async getAll(type?: string): Promise<ISeason[]> {
+    const { data } = await api.get<ISeason[]>("/seasons", {
+      params: type ? { type } : undefined,
+    });
+    return data;
+  },
+  async getOne(id: number): Promise<ISeason> {
+    const { data } = await api.get<ISeason>(`/seasons/${id}`);
+    return data;
+  },
+
+  async create(data: Partial<ISeason>): Promise<ISeason> {
+    const { data: createdSeason } = await api.post<ISeason>(
+      "/seasons",
+      data,
+    );
+    return createdSeason;
+  },
+};
