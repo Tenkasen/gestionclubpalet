7f9712294d27d86808687bbad2595736f8a14584
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-02-26 15:56:36 +0100

    chore : add .env file and update gitignore

--- gestionclubpalet-front/src/api/client.ts
+++ gestionclubpalet-front/src/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const instance = axios.create({
+const api = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 1000,
   headers: { "Content-Type": "application/json" },
