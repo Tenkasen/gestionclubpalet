e0641a0e80974e061b4b58d1ce3b29f6630c9024
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-03-03 15:42:04 +0100

    feat : begin dashboard page

--- gestionclubpalet-front/src/hooks/useSeasons.ts
+++ gestionclubpalet-front/src/hooks/useSeasons.ts
@@ -1,37 +1,29 @@
 import { useEffect, useState } from "react";
-
 import { seasonsAPI } from "../api/season.api";
 import type { ISeason } from "../types";
 
+export function useSeasons(type?: string) {
+  const [seasons, setSeasons] = useState<ISeason[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
 
-
-export default function useSeasons(type?:string) {
-
-    const [seasons, setSeasons] = useState<ISeason[]>([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState<string | null>(null)
-
-    const fetchSeasons = () => {
-        try {
-            setLoading(true)
-            const response = await seasonsAPI.getAll(type)
-            setSeasons(response)
-            setError(null)
-        } catch (error) {
-            setError('Erreur lors du chargement des saisons')
-            console.error(error)
-        }finally{
-            setLoading(false)
-        }
-
-         useEffect (() =>{
-            fetchSeasons()
-        },[type])
-
-        return {seasons, loading, error, refetch : fetchSeasons}
+  const fetchSeasons = async () => {
+    try {
+      setLoading(true);
+      const response = await seasonsAPI.getAll(type);
+      setSeasons(response);
+      setError(null);
+    } catch (error) {
+      setError("Erreur lors du chargement des saisons");
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-    
-  return (
-    <div>useSeasons</div>
-  )
+  };
+
+  useEffect(() => {
+    fetchSeasons();
+  }, [type]);
+
+  return { seasons, loading, error, refetch: fetchSeasons };
 }
---
+++ gestionclubpalet-front/src/pages/Dashboard.tsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+import { useSeasons } from "../hooks/useSeasons";
+
+export default function Dashboard() {
+  const { seasons, loading, error } = useSeasons();
+
+  if (loading) return <div>"Chargement en cours"</div>;
+  if (error) return <div>{error}</div>;
+
+  return (
+    <div>
+      <div>
+        <h1>Gestion Palet</h1>;
+        <div>
+          <Link to="/">
+            <div>🎯</div>
+            <h3>Entraînement</h3>
+          </Link>
+          <Link to="/">
+            <div>🏅</div>
+            <h3>Championnat</h3>
+          </Link>
+          <Link to="/">
+            <div>🏆</div>
+            <h3>Coupe</h3>
+          </Link>
+        </div>
+      </div>
+
+      <h2>Saison active</h2>
+      {seasons.length === 0 ? (
+        <p>Aucune saison crée</p>
+      ) : (
+        <ul>
+          {seasons.map((season) => (
+            <li>
+              <h3>{season.nom}</h3>
+              <p>{season.type}</p>
+              <p>
+                {new Date(season.dateDebut).toLocaleDateString(
+                  "fr-FR",
+                )}
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
