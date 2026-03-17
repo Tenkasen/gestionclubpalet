db711be2fd6fa6fc8ef770782afcffa7697a70c7
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-03-17 16:04:18 +0100

    feat : add a day check in training scores controller

--- gestionclubpalet-api/app/controllers/training_scores_controller.ts
+++ gestionclubpalet-api/app/controllers/training_scores_controller.ts
@@ -1,9 +1,20 @@
+import Day from '#models/day'
 import TrainingScore from '#models/training_score'
 import { createTrainingScoreValidator } from '#validators/training_score'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class TrainingScoresController {
-  async index({ params }: HttpContext) {
+  async index({ params, response }: HttpContext) {
+    const existingDay = await Day.query()
+      .where('season_id', params.seasonId)
+      .where('id', params.dayId)
+      .first()
+
+    if (!existingDay) {
+      return response.notFound({
+        message: `Cette journée n'existe pas dans cette saison`,
+      })
+    }
     const scores = await TrainingScore.query().where('day_id', params.dayId).preload('player')
     return scores
   }
