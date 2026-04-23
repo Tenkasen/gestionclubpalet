b81256f375855d692c5eecaa1dae52f6f2e775ec
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-04-23 15:17:58 +0200

    feat : ranking interface

---
+++ gestionclubpalet-front/src/types/ranking.ts
@@ -0,0 +1,11 @@
+export interface IRanking {
+  playerId: number;
+  nom: string;
+  prenom: string;
+  presences: number;
+  totalPour: number;
+  totalContre: number;
+  goalAverage: number;
+  points: number;
+  position: number;
+}
