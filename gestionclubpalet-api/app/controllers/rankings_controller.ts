c79877565397b0d992abd4ff57903e12ff1d65e2
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-04-24 15:47:12 +0200

    feat : add a check to know if the day is created in the season in ranking route

--- gestionclubpalet-api/app/controllers/rankings_controller.ts
+++ gestionclubpalet-api/app/controllers/rankings_controller.ts
@@ -1,12 +1,23 @@
 import type { HttpContext } from '@adonisjs/core/http'
 
 import RankingService from '#services/ranking_services'
+import Day from '#models/day'
 // import Day from '#models/day'
 
 export default class RankingsController {
   private rankingService = new RankingService()
 
-  async dayRanking({ params }: HttpContext) {
+  async dayRanking({ params, response }: HttpContext) {
+    const existingDay = await Day.query()
+      .where('season_id', params.seasonId)
+      .where('id', params.dayId)
+      .first()
+
+    if (!existingDay) {
+      return response.notFound({
+        message: `Cette journée n'existe pas dans cette saison`,
+      })
+    }
     // call function with upToDay
     const ranking = await this.rankingService.getTrainingRanking(params.seasonId, params.dayId)
 
