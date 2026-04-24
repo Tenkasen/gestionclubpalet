0d8d273c1e2f5981e9c4e48bbc2bfed455ca7879
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-04-24 16:03:41 +0200

    feat : hook useRankings

--- gestionclubpalet-front/src/hooks/useRankings.ts
+++ gestionclubpalet-front/src/hooks/useRankings.ts
@@ -1,23 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { IRanking } from "../types/ranking";
+import { rankingApi } from "../api/ranking.api";
 
 export default function useRankings(
   seasonId: number,
   dayId?: number,
 ) {
-  const [ranking, setRanking] = useState<IRanking[]>([]);
+  const [ranking, setRanking] = useState<IRanking[] | null>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
 
-
-  const fetchRanking = async(){
+  const fetchRanking = async () => {
     try {
-        setLoading(true)
-        if(dayId){
-            const dayRanking = await 
-        }
+      setLoading(true);
+      if (dayId) {
+        const response = await rankingApi.getDayRanking(
+          seasonId,
+          dayId,
+        );
+        setRanking(response);
+      }
+
+      const response = await rankingApi.getSeasonRanking(seasonId);
+      setRanking(response);
+      setError(null);
     } catch (error) {
-        
+      setError("Erreur lors du chargement des saisons");
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
-  }
+  };
+
+  useEffect(() => {
+    fetchRanking();
+  }, [seasonId, dayId]);
+
+  return { ranking, loading, error, refetch: fetchRanking };
 }
