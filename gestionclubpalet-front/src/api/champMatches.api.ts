4341a6371a112452721e5ef4dc64e39389bfa7d4
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-05-05 09:46:20 +0200

    feat : front api create champ matches

--- gestionclubpalet-front/src/api/champMatches.api.ts
+++ gestionclubpalet-front/src/api/champMatches.api.ts
@@ -1,5 +1,6 @@
 import { isAxiosError } from "axios";
 import type { IChampMatches } from "../types/champMatches.ts";
+import { api } from "./client.ts";
 
 export const champMatchesApi = {
   async getAll(
@@ -7,7 +8,7 @@ export const champMatchesApi = {
     dayId: number,
   ): Promise<IChampMatches[]> {
     try {
-      const { data } = await api.get<IChampMatches>(
+      const { data } = await api.get<IChampMatches[]>(
         `/seasons/${seasonId}/days/${dayId}/champ-matches`,
       );
       return data;
@@ -17,6 +18,30 @@ export const champMatchesApi = {
       }
       console.error(
         "Erreur lors de la récupération des inscriptions à cette saison (rankingApi.getDayRanking)",
+        error,
+      );
+      throw error;
+    }
+  },
+
+  async create(
+    seasonId: number,
+    dayId: number,
+    data: Partial<IChampMatches>,
+  ): Promise<IChampMatches | null> {
+    try {
+      const { data: createdChampMatches } =
+        await api.post<IChampMatches>(
+          `/seasons/${seasonId}/days/${dayId}/champ-matches`,
+          data,
+        );
+      return createdChampMatches;
+    } catch (error: unknown) {
+      if (isAxiosError(error) && error.response?.status === 404) {
+        throw new Error(`La saison ou la journée n'existe pas`);
+      }
+      console.error(
+        "Erreur lors de la récupération des inscriptions à cette saison (champMatchesApi.create):",
         error,
       );
       throw error;
