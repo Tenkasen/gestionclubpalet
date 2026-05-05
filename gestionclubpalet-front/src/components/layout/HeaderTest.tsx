504aa6e043d49156d2b50d43f8d198f3c93322bc
Mickael-Bourdet <mickael.brdt@gmail.com>
2026-05-05 20:07:21 +0200

    feat : header with test links

---
+++ gestionclubpalet-front/src/components/layout/HeaderTest.tsx
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <header className="border-b py-4 border-indigo-800 sticky top-0 z-90 bg-white">
+      <div className="flex justify-center gap-12 text-lg items-center ">
+        <Link
+          to="/"
+          className="hover:underline
+        hover:text-indigo-800 text-gray-700"
+        >
+          Accueil
+        </Link>
+        <Link
+          to="/saisons/7/journées/1/saisie-entrainement"
+          className="hover:underline
+        hover:text-indigo-800 text-gray-700"
+        >
+          Saisie Entraînement
+        </Link>
+        <Link
+          to="/saisons/8/journées/1/saisie-match"
+          className="hover:underline
+        hover:text-indigo-800 text-gray-700"
+        >
+          Saisie Championnat
+        </Link>
+        <Link
+          to="/test"
+          className="hover:underline
+        hover:text-indigo-800 text-gray-700"
+        >
+          Test
+        </Link>
+        <Link
+          to="/classements/7/journées/1"
+          className="hover:underline
+        hover:text-indigo-800 text-gray-700"
+        >
+          Classement
+        </Link>
+      </div>
+    </header>
+  );
+}
